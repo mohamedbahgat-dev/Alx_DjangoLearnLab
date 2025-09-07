09065a70da6ecc47ed9aa0c2636ef5921077fe78
mohamedbahgat <mohamedbahgat950@gmail.com>
2025-09-07 08:18:13 +0300

    create relationship-app and library-models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,11 +1,11 @@
 from models import Book, Librarian, Library
 
 # list all books in alibrary
-books = Library.objects.get(name='london')
+books = Library.objects.get(name='library_name')
 books.all()
 
 # Filtering books by author
-books_by_author = Book.objects.filter(author='Sharles Dekens')
+books_by_author = Book.objects.filter(author='author_name')
 
 # retrive librarian for specific library
-librarian_for_library = Librarian.objects.filter(library = 'New York')
+librarian_for_library = Librarian.objects.filter(library = 'library_name')
