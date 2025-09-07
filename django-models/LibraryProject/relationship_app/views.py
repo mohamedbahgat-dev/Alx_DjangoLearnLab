ca92029f3fb41afbaac4f8a0bf2c221084c4df0b
mohamedbahgat <mohamedbahgat950@gmail.com>
2025-09-07 12:03:20 +0300

    create view that retrive library model

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from .models import Book
 from .models import Library
 
 from django.views.generic import ListView
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 
 
 # Create your views here.
