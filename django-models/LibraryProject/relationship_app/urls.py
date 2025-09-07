6e2947cc69b2dae1f0e85196c223532bb655699b
mohamedbahgat <mohamedbahgat950@gmail.com>
2025-09-07 13:59:42 +0300

    create user authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
-from .views import list_books ,  LibraryDetailView
+from django.urls import path, include
+from .views import list_books ,  LibraryDetailView , LogInForm, SignUpForm, ProfileTemplate
 
 urlpatterns = [
     path('books/', list_books, name='books'),
-    path('library/', LibraryDetailView.as_view(), name='library' )
+    path('library/', LibraryDetailView.as_view(), name='library' ),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login', LogInForm.as_view(), name='login'  ),
+    path('accounts/profile/', ProfileTemplate.as_view(), name='profile'),
+    path('register', SignUpForm.as_view(), name='register'  ),
+  
 
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import Book
 from .models import Library
-
+from django.contrib.auth.views import LoginView
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic import TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 
 # Create your views here.
@@ -19,3 +23,17 @@ class LibraryDetailView(DetailView):
     model = Library
     context_object_name = 'library'
     
+class LogInForm(LoginView):
+    template_name = 'relationship_app/templates/login.html'
+
+class ProfileTemplate(TemplateView):
+    template_name = 'relationship_app/templates/profile.html'
+
+
+class SignUpForm(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = 'relationship_app/templates/register.html'
+
+class ProfileTemplate(TemplateView):
+    template_name = 'relationship_app/templates/profile.html'
