e9bca989db015b53b95d9a93aea4f64787e1a032
mohamedbahgat <mohamedbahgat950@gmail.com>
2025-09-08 08:08:26 +0300

    create user authentication

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,8 @@ from django.views.generic import TemplateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -32,3 +34,15 @@ def register(request):
         form = UserCreationForm()
     return render(request, "relationship_app/register.html", {"form": form})
 
+def is_admin(user):
+    return bool(getattr(user, "is_authenticated", False) and
+                getattr(user, "profile", None) and
+                user.profile.role == 'admin')
+
+@user_passes_test(is_admin, login_url='login', redirect_field_name='next')
+def admin_view(request):
+    return HttpResponse("Admin-only content")
+
+admin_template = 'relationship_app/admin_view.html'
+librarian_template = 'relationship_app/librarian_view.html'
+member_template = 'relationship_app/member_view.html'
