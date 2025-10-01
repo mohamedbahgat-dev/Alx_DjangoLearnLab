94e9cf1eed243aff5905f2be81674bc91c14cdfd
mohamedbahgat <mohamedbahgat950@gmail.com>
2025-10-01 12:47:24 +0300

    Implementing the Blog's User Authentication System

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import BlogHomeView, PostListView, LogInView
+from django.contrib.auth.views import LogoutView
+from .views import SignUpView
+from .views import ProfileView
+
+
+urlpatterns = [
+    path('', BlogHomeView.as_view(), name= 'home' ),
+    path('posts/',PostListView.as_view(), name='posts' ),
+    path('login', LogInView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
+    path('register', SignUpView.as_view(), name='register'),
+    path('profile', ProfileView.as_view(), name = 'profile')
+
+]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.views.generic import ListView, TemplateView
+from django.contrib.auth.views import LoginView
+from .forms import CustomeCreationFrom
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .models import Post
 
 # Create your views here.
+class SignUpView(CreateView):
+    form_class = CustomeCreationFrom
+    success_url = reverse_lazy('login')
+    template_name = 'blog/signup.html'
+
+    
+class LogInView(LoginView):
+    template_name = 'blog/login.html'
+
+class BlogHomeView(TemplateView):
+    model = Post
+    template_name = 'blog/home.html'
+   
+    
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/posts.html'
+    context_object_name = 'posts'
+
+class ProfileView(TemplateView):
+    template_name = 'blog/profile.html'
