f254065a01ccff42d87d04cf423eb0d72b85de01
mohamedbahgat <mohamedbahgat950@gmail.com>
2025-10-01 13:39:43 +0300

    Implementing the Blog's User Authentication System

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import ModelForm
 
 class CustomeCreationFrom(UserCreationForm):
     class Meta:
         model = User
         fields = ('username','email',)
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,14 @@
 from django.views.generic import ListView, TemplateView
 from django.contrib.auth.views import LoginView
-from .forms import CustomeCreationFrom
+from .forms import CustomeCreationFrom, ProfileForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from .models import Post
+from django.views import View
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
 class SignUpView(CreateView):
@@ -25,5 +30,18 @@ class PostListView(ListView):
     template_name = 'blog/posts.html'
     context_object_name = 'posts'
 
-class ProfileView(TemplateView):
+class ProfileView(View, LoginRequiredMixin):
     template_name = 'blog/profile.html'
+
+    def get(self, request):
+        form = ProfileForm(instance=request.user)
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request):
+        method = request.POST
+        form = ProfileForm(method, instance = request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile has been updated successfuly!')
+            return redirect('profile')
+        return render(request, self.template_engine, {'form':form})
