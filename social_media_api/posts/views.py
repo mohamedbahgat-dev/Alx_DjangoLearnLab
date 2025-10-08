6f9c165e2e968c47422b4e879325a42dff084adb
mohamedbahgat <mohamedbahgat950@gmail.com>
2025-10-08 09:04:43 +0300

    mplementing User Follows and Feed Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -41,5 +41,5 @@ class FeedView(generics.ListAPIView):
 
     def get_queryset(self):
         user = self.request.user
-        following_user = user.following.all()
-        return Post.objects.filter(author__in=following_user).order_by('-published_date')
+        following_users = user.following.all()
+        return Post.objects.filter(author__in=following_users).order_by('-published_date')
